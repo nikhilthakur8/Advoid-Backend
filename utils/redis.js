ab36bf345ae150ca1576c3be2dedeab0f15c6657
nikhilthakur8 <nikhilthakur8012004@gmail.com>
2025-12-31 01:00:05 +0530

    fixed the redis scoket error

--- utils/redis.js
+++ utils/redis.js
@@ -3,13 +3,18 @@ const { createClient } = require("redis");
 const client = createClient({
 	url: process.env.REDIS_URL,
 	socket: {
-		reconnectStrategy: (retries) => Math.min(retries * 50, 500),
+		reconnectStrategy: (retries) => {
+			return Math.min(retries * 100, 3000);
+		},
 	},
 });
 
 async function connectRedis() {
 	try {
 		await client.connect();
+		client.on("error", (err) => {
+			console.error("Redis Client Error", err);
+		});
 		console.log("Connected to Redis");
 	} catch (error) {
 		console.log("Redis connection error:", error);
